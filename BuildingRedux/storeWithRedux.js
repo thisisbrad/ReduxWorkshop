f1c73e545b3a1045348001488abf9d21b8d9588d
Beltowski <bradbeltowski@gmail.com>
2019-08-29 13:54:12 -0400

    start step 09 with Redux

---
+++ BuildingRedux/storeWithRedux.js
@@ -0,0 +1,59 @@
+const redux = require('redux');
+
+const DEFAULT_STATE = { user: {}, contacts: [] };
+const UPDATE_USER = 'UPDATE_USER';
+const UPDATE_CONTACT = 'UPDATE_CONTACT';
+
+const userReducer = (state = {}, action) => {
+  if (action.type === UPDATE_USER) return { ...state, ...action.payload };
+  // if (action.type === UPDATE_CONTACT)
+  //   return { ...state, recentContact: action.payload };
+  return state;
+};
+
+const contactReducer = (state = [], action) => {
+  if (action.type === UPDATE_CONTACT) return [...state, action.payload];
+  return state;
+};
+
+const reducer = (state, action) => ({
+  user: userReducer(state.user, action),
+  contacts: contactReducer(state.contact, action)
+});
+
+class Store {
+  constructor(reducer, initialState) {
+    this.reducer = reducer;
+    this.state = initialState;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  dispatch(update) {
+    this.state = this.reducer(this.state, update);
+  }
+}
+
+const store = new Store(reducer, DEFAULT_STATE);
+
+const updateUser = update => ({ type: UPDATE_USER, payload: update });
+
+const addContact = newContact => ({
+  type: UPDATE_CONTACT,
+  payload: newContact
+});
+
+store.dispatch(updateUser({ name: 'Rick' }));
+store.dispatch(updateUser({ status: 'Getting Swifty' }));
+store.dispatch(updateUser({ name: 'Morty' }));
+// store.dispatch({ type: UPDATE_USER, payload: { name: 'Morty' } });
+
+store.dispatch(addContact({ name: 'Morty', number: '123456789' }));
+// store.dispatch({
+//   type: UPDATE_CONTACT,
+//   payload: { name: 'Morty', number: '123456789' }
+// });
+
+console.log(store.getState());
